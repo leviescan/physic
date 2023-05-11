4ff9874cd1e466ab9e8f18f2439a558a2e1c8aed
AndresPortoCastro <101732746+leviescan@users.noreply.github.com>
2023-05-11 18:39:42 -0500

    final

---
+++ src/app/suport/suport.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suport',
+  templateUrl: './suport.component.html',
+  styleUrls: ['./suport.component.css']
+})
+export class SuportComponent {
+
+}
